7f911e261ad2345781af59ed341332339b5d2bda
shimu <4cho.kaigi@gmail.com>
2016-08-24 00:56:26 +0900

    練習問題の修正

--- kiwami_javascript/src/js/kiwami_lesson3.js
+++ kiwami_javascript/src/js/kiwami_lesson3.js
@@ -2,7 +2,27 @@
  javascript 極み
 =================================================================== */
 /* ====回答結果を実行======*/
-function answer_build(elmId) {
+function answer_build_1(elmId) {
+ $.ajax({
+      type: 'POST',
+      url: 'http://zipcloud.ibsnet.co.jp/api/search',
+      async: true,
+      dataType: 'jsonp',
+      data: {
+       'zipcode': '1510053'
+      },
+	  success: function(result){
+	    var obj = result.results[0];
+	    var address = obj.address1 + obj.address2 + obj.address3;
+        $("#" + elmId).text(address).css('background-color', 'cyan');
+      },
+      error: function(result){
+       alert(result)
+      }
+     });
+}
+
+function answer_build_2(elmId) {
  $.ajax({
       type: 'GET',
       url: 'http://www.ekidata.jp/api/p/13.json',
@@ -21,7 +41,12 @@ function answer_build(elmId) {
 }
 
 /* ====実行結果をリセット======*/
-function answer_reset(elmId) {
+function answer_reset_1(elmId) {
+	var sec = "#" + elmId;
+	$(sec).text("実行結果を表示").css('background-color', 'white');;
+}
+
+function answer_reset_2(elmId) {
 	var sec = "#" + elmId;
 	$(sec).find("option").remove();
 	var option = $('<option >').attr('value', "").text("選択してください");
